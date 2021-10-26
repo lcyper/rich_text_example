bad35ad7b8b05c89ff5055c1031ddb6520793d53
lcyper <lcyper@gmail.com>
2021-10-26 14:08:43 -0300

    add googleFonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:rich_text_example/screens/home_screen.dart';
 
 void main() {
@@ -15,6 +16,9 @@ class MyApp extends StatelessWidget {
       title: 'RichText Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.rubikTextTheme(
+            // Theme.of(context).textTheme,
+            ),
       ),
       home: HomeScreen(),
     );
